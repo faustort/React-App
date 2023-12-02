54638244e86ce5139c28372ab31b239b6392fcaf
Your Name <you@example.com>
2023-12-01 21:23:08 -0300

    correcoes

--- src/assets/db/context.js
+++ src/assets/db/context.js
@@ -1,44 +1,49 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.use(cors());
 app.use(express.json());
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'teste123',
-    database: 'aeo'
+    database: 'aeo',
+    port: 3306
+
 });
 
-connection.connsect((err) => {
-    if(err){
-        console.error('Erro ao conectar ao DB: ' + err.message);
-    } else{
-        console.log('Conectado ao DB');
-    }
-})
+// connection.connection((err) => {
+//     if (err) {
+//         console.error('Erro ao conectar ao DB: ' + err.message);
+//     } else {
+//         console.log('Conectado ao DB');
+//     }
+// })
 
 app.get('/aluno', (req, res) => {
-    connection.query('SELECT * FROM alunos', (err, results) => {
-        if(err){
-            res.status(500).json({error: 'Erro ao obter alunos no banco de dados'});
-        } else{
-            res.json(results);
-        }
-    })
+    connection.getConnection((err, con) => {
+
+        con.query('SELECT * FROM alunos', (err, results) => {
+            if (err) {
+                res.status(500).json({ error: 'Erro ao obter alunos no banco de dados' });
+            } else {
+                res.json(results);
+            }
+        })
+    });
 })
 
 app.post('/aluno', (req, res) => {
-    const {nome, idade, telefone} = req.body;
+    const { nome, idade, telefone } = req.body;
     connection.query('INSERT INTO alunos (nome, idade, telefone) VALUES (?, ?, ?)', [nome, idade, telefone], (err, results) => {
-        if(err){
-            res.status(500).json({error: 'Erro ao inserir aluno no banco de dados'});
-        } else{
+        if (err) {
+            res.status(500).json({ error: 'Erro ao inserir aluno no banco de dados' });
+        } else {
             res.json(results);
         }
     })
